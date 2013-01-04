f40da7b5ce76256ff023e91ecf8346e83ed7adde
Liukai <liukai@shendu.com>
2013-01-04 17:24:09 +0800

    Add msm7x27a audio HAL
    Change-Id: I4c11c89ec735d20c7eb8b00109af94f81abb62d9

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm7x27a,$(TARGET_BOARD_PLATFORM)),)
 ifeq ($(TARGET_QCOM_AUDIO_VARIANT),)
 AUDIO_ROOT := $(call my-dir)
 include $(call all-subdir-makefiles)
